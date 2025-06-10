38531bb3bba1220dc60a7edd5f82e9da72ea457a
pragnesh05 <rachna_0705@yahoo.com>
2025-06-10 12:52:26 -0400

    Base code

---
+++ myfirstproject/src/test/java/seleniumpack/NavigateUrl.java
@@ -0,0 +1,30 @@
+package seleniumpack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class NavigateUrl {
+    public static void main(String[] args) throws InterruptedException{
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+        String URL1 = driver.getCurrentUrl();
+        System.out.println(URL1);
+Thread.sleep(3000);
+driver.navigate().to("https://www.selenium.dev/");
+Thread.sleep(3000);
+        System.out.println(driver.getCurrentUrl());
+        driver.navigate().back();
+        Thread.sleep(3000);
+        System.out.println(driver.getCurrentUrl());
+        driver.navigate().forward();
+        Thread.sleep(3000);
+        System.out.println(driver.getCurrentUrl());
+        driver.navigate().refresh();
+        Thread.sleep(3000);
+        System.out.println(driver.getCurrentUrl());
+        driver.close();
+
+    }
+}
---
+++ myfirstproject/src/test/java/seleniumpack/SaleForceLogin.java
@@ -0,0 +1,32 @@
+package seleniumpack;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SaleForceLogin {
+    public static void main(String[] args) throws InterruptedException{
+
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://login.salesforce.com/");
+        Thread.sleep(3000);
+        driver.manage().window().maximize();
+        driver.findElement(By.id("username")).sendKeys("komal");
+        Thread.sleep(2000);
+        driver.findElement(By.name("pw")).sendKeys("1234");
+        Thread.sleep(2000);
+        driver.findElement(By.id("Login")).click();
+       String output= driver.findElement(By.xpath("//div[@id='error']")).getText();
+        System.out.println(output);
+        Thread.sleep(2000);
+
+        driver.close();
+
+
+
+
+    }
+
+    }
+

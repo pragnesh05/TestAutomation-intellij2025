97831920f52f53652a9c4e7b3b69a4a948aee4e6
pragnesh05 <rachna_0705@yahoo.com>
2025-06-13 13:34:58 -0400

    loopexample

--- myfirstproject/src/test/java/javabasics/ArrayExample.java
+++ myfirstproject/src/test/java/javabasics/ArrayExample.java
@@ -1,4 +1,14 @@
 package javabasics;
 
 public class ArrayExample {
+    public static void main(String[] args) {
+int[] i={1,2,3,4,5};
+        System.out.println(i[0]);
+        System.out.println(i[4]);
+        System.out.println(i.length);
+        String[] name={"rachna","reema","rinku"};
+        System.out.println(name[1]);
+    }
+
 }
+
---
+++ myfirstproject/src/test/java/javabasics/LoopsExample.java
@@ -0,0 +1,12 @@
+package javabasics;
+
+public class LoopsExample {
+    public static void main(String[] args) {
+        int[] a= {10,20,30,40};
+        for(int h=0; h < a.length; h++)
+        {
+            System.out.println(a[h]);
+        }
+
+    }
+}
